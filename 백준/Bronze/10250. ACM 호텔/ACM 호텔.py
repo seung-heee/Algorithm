6029154f82e69cf960f6e37d48ce20990e1b8249
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-05-12 21:50:08 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,13 @@
+import sys
+
+T = int(input())
+
+for _ in range(T):
+  H, W, N = map(int, sys.stdin.readline().split())
+  floor = N % H
+  room = N // H + 1
+  
+  if (floor == 0):
+    floor = H
+    room -= 1
+  print(floor*100 + room)
