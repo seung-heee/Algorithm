13b272f62c4ef07ccff6f180259ae39c29d39fef
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-06-18 10:24:26 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -2,16 +2,15 @@ import java.util.*;
 public class Main{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		int a;
 		int[] arr = new int[31];
 		
-		for (int i=1; i<29; i++) {
-			a = sc.nextInt();
+		for (int i = 0; i < 28; i++) {
+			int a = sc.nextInt();
 			arr[a] = 1;
 		}
 		
 		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] != 1) System.out.println(i);
+			if (arr[i] == 0) System.out.println(i);
 		}
 	}
 }
