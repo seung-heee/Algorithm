7af6732130db239f724a417165351f34c635dde6
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-07-02 17:34:34 +0900

    [Silver III] Title: 통계학, Time: 460 ms, Memory: 56412 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+
+for i in range(N):
+  arr.append(int(input()))
+
+avg = round((sum(arr) / len(arr))) # 산술 평균
+mid = sorted(arr)[len(arr) // 2] # 중앙값 : 정렬하고, 중앙에 있는 값
+max_count = 0
+max_min_diff = max(arr) - min(arr)
+
+print(avg)
+print(mid)
+
+dict = {}
+for a in arr:
+  if a not in dict:
+    dict[a] = 1
+  else:
+    dict[a] += 1
+
+max_cnt = max(dict.values())
+max_arr = sorted([k for k, v in dict.items() if v == max_cnt])
+
+if len(max_arr) > 1:
+  print(max_arr[1])
+else:
+  print(max_arr[0])
+
+print(max_min_diff)
