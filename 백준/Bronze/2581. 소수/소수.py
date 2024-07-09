72daae3ba11cb8c8a4eb719f0a268dafb995ae4e
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-07-09 13:40:37 +0900

    [Bronze II] Title: 소수, Time: 576 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+N = int(input())
+sosu = []
+    
+for num in range(M, N+1):
+  not_prime = 0
+  if num > 1:
+    for j in range(2, num):
+      if (num % j == 0): # 소수가 아니면
+        not_prime += 1
+        break
+    if not_prime == 0:
+      sosu.append(num)
+
+if len(sosu) > 0:
+  print(sum(sosu))
+  print(sosu[0])
+else:
+  print(-1)
