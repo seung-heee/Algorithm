e30d08167cb6cfdec44472831d4aaf0000fefb65
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-07-14 15:47:29 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 명령의 수
+arr = []
+
+for i in range(N):
+  cmd = input().split()
+
+  if cmd[0] == 'push':
+    arr.append(cmd[1])
+
+  elif cmd[0] == 'pop':
+    if not arr:
+      print(-1)
+    else: 
+      print(arr.pop())
+
+  elif cmd[0] == 'size':
+    print(len(arr))
+
+  elif cmd[0] == 'empty':
+    if not arr:
+      print(1)
+    else: 
+      print(0)
+
+  elif cmd[0] == 'top':
+    if not arr:
+      print(-1)
+    else:
+      print(arr[-1])
