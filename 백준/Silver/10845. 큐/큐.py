cf79aa1a86c67d5da7b49476cce2858c0d9101fb
seung-heee <112852049+seung-heee@users.noreply.github.com>
2024-07-15 21:46:25 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+queue = []
+
+for _ in range(N):
+  cmd = input().rstrip().split()
+  c = cmd[0]
+
+  if c == 'push':
+    queue.append(int(cmd[1]))
+  elif c == 'pop':
+    if not queue: print(-1)
+    else:
+      print(queue.pop(0))
+  elif c == 'size':
+    print(len(queue))
+  elif c == 'empty':
+    if not queue: print(1)
+    else: print(0)
+  elif c == 'front':
+    if not queue: print(-1)
+    else: print(queue[0])
+  elif c == 'back':
+    if not queue: print(-1)
+    else: print(queue[-1])
